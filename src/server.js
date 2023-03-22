a1b3f7ea078b4083e0d0cd0a707ed5da4d11e9cc
ismaelnaciri <114733902+ismaelnaciri@users.noreply.github.com>
2023-03-22 09:01:30 +0100

    1st commit

---
+++ src/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+const cors=require('cors');
+app.use(cors())
+app.use(express.json());
+port = 3080;
+
+app.listen(port, ()=>{
+  console.log(`el port::${port} funciona`)
+});
+
+
+app.get('/api/login',(req,res)=> {
+  var serviceAccount = require("./botiga-61177-firebase-adminsdk-a1p5h-9b6614abd8.json");
+  var admin = require("firebase-admin");
+  const {getFirestore} = require("firebase-admin/firestore")
+  const app = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+  const db = getFirestore(app);
+  db.collection('iniciar-registrar').doc('8X8qyKzfzPYPdqlUA6Ut').get().then(al2 =>{
+    console.log(al2);
+    res.json(al2);
+  });
+
+
+  // admin.initializeApp({
+  //   credential: admin.credential.cert(serviceAccount)
+  // });
+});
+
+
