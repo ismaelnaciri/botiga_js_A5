96cdcf6aa926b18aa623f114939749c083a82b4e
ismaelnaciri <114733902+ismaelnaciri@users.noreply.github.com>
2023-03-23 08:10:19 +0100

    1st commit

--- src/server.js
+++ src/server.js
@@ -12,22 +12,26 @@ app.listen(port, ()=>{
 
 
 app.get('/api/login',(req,res)=> {
+
   var serviceAccount = require("./botiga-61177-firebase-adminsdk-a1p5h-9b6614abd8.json");
   var admin = require("firebase-admin");
-  const {getFirestore} = require("firebase-admin/firestore")
+  const {getFirestore} = require("firebase-admin/firestore");
   const app = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
   });
   const db = getFirestore(app);
-  db.collection('iniciar-registrar').doc('8X8qyKzfzPYPdqlUA6Ut').get().then(al2 =>{
-    console.log(al2);
-    res.json(al2);
-  });
-
-
-  // admin.initializeApp({
-  //   credential: admin.credential.cert(serviceAccount)
+  // db.collection('iniciar-registrar').doc('8X8qyKzfzPYPdqlUA6Ut').get().then(al2 =>{
+  //   console.log(al2);
+  //   res.json(al2);
   // });
+
+  console.log(req.query);
+
+  // const res ={};
+
+  db.collection('iniciar-registrar').doc('8X8qyKzfzPYPdqlUA6Ut').set(req.query).then(r => {
+    console.log("dades inserides");
+  })
 });
 
 
