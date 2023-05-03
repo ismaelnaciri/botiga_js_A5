56fc82b6198bac7d6c3b547f47a46132242f6dbc
diazdani <daniel.diaz@institutvidreres.cat>
2023-05-03 18:20:39 +0200

    Commit BASE DE DADES PRODUCTES

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+// @ts-ignore
 import { TestComponent } from './test/test.component';
+import { ActivitatsComponent } from './activitats/activitats.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    ActivitatsComponent
   ],
   imports: [
     BrowserModule
--- src/server.js
+++ src/server.js
@@ -1,20 +1,82 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config({ path: '../.env' });
+
 const app = express();
+
 const {FieldValue} = require('firebase-admin/firestore')
 const cors=require('cors');
 const fs = require("fs");
 
-app.use(cors())
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  }
+);
+
+sequelize.authenticate().then(() => {
+  console.log('Conexió establerta');
+}).catch((error) => {
+  console.error("No s'ha pogut connectar", error);
+});
+
+const Product = sequelize.define("productes", {
+  idproducte:{
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },nom: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },preu:{
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },img:{
+    type: Sequelize.STRING,
+    allowNull: false
+  },tipus:{
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+sequelize.sync().then(()=>{
+  console.log('Base de dades sincroniotzada');
+}).catch((error) => {
+  console.error("No s'ha pogut sincronitzar", error);
+})
+
+app.use(cors());
 app.use(express.json());
+
 port = 3080;
 
 app.listen(port, ()=>{
   console.log(`el port::${port} funciona`)
 });
 
+app.get('/productes', async (req, res) => {
+
+  Product.findAll().then((data)=>{
+    res.json(data)
+  }).catch((error)=>{
+    console.error("Han fallat els productes", error)
+  })
+
+})
+
+
 var admin = require("firebase-admin");
 var serviceAccount = require("./botiga-61177-firebase-adminsdk-a1p5h-9b6614abd8.json");
 const {getFirestore} = require("firebase-admin/firestore");
+const {request} = require("express");
+const {DataTypes} = require("sequelize");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -23,7 +85,7 @@ const db = getFirestore(app);
 
 baseDades();
 async function baseDades() {
-  const conn = db.collection("iniciar-registrar").doc("8X8qyKzfzPYPdqlUA6Ut");
+
 
   app.get('/api/firebase', async (req, res) => {
     const conn = db.collection("iniciar-registrar").doc("8X8qyKzfzPYPdqlUA6Ut");
@@ -64,6 +126,7 @@ app.get('/imatges/:nom',(req,res)=>{
   const stream = fs.createReadStream(rutaImatge);
   stream.pipe(res);
 })
+
 
 
 /*
